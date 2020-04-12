b49a04512aee5b912a2c4ca1a10ebfc269fafd0d
TamasGorgics <toma2067@gmail.com>
2020-04-12 13:26:38 +0200

    Create initial Spring Boot project

---
+++ src/test/kotlin/com/gorgics/apikeyplatform/ApikeyPlatformApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gorgics.apikeyplatform
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApikeyPlatformApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
